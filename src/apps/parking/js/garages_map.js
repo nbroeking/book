dff7e765186f8150e2f3273421dcc9e47a297d17
Nicolas Broeking <nbroeking@me.com>
2016-02-01 16:52:58 -0700

    Merging

--- src/apps/parking/js/garages_map.js
+++ src/apps/parking/js/garages_map.js
@@ -2,22 +2,25 @@ $(document).ready(function() {
   var ref = new Firebase('https://publicdata-parking.firebaseio.com');
   var data;
   
-  var busesRef = new Firebase('https://publicdata-transit.firebaseio.com/sf-muni');
-  
   var transitRef = new Firebase('https://publicdata-transit.firebaseio.com/sf_muni');
-  var lineIndex = transitRef.child('index').child(transitLine);
-    lineIndex.on('child_added', function(snapshot) {
+  var lineIndex = transitRef.child('sf-muni/vehicles');
+  lineIndex.on('child_added', function(snapshot) {
+
+    console.log('Following bus', snapshot)
     var id = snapshot.key();
-    transitRef.child('data').child(id).on('value', busUpdated);
+    transitRef.child(id).on('value', busUpdated);
   });
+  
   lineIndex.on('child_removed', function(snapshot) {
     var id = snapshot.key();
-    transitRef.child('data').child(id).off('value', busUpdated);
+    transitRef.child(id).off('value', busUpdated);
   });
 
   function busUpdated(snapshot) {
     // Bus line 'X' changed location.
     var info = snapshot.val();
+    
+    console.log("Bux changed location", routeTag);
     // Retrieve latitude/longitude with info.lat/info.lon.
   }
   
