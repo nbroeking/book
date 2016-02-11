11a12bfc244915569ef0e9ffd893953e0aab6ea1
Nicolas Broeking <nbroeking@me.com>
2016-02-10 18:46:47 -0700

    Clean Repo

--- src/apps/uber/client/components/app.jsx
+++ src/apps/uber/client/components/app.jsx
@@ -1,21 +1,25 @@
 class App extends React.Component {
   render(){
-    return <div>
-      <MyComponents.NavBar actions={this.props.actions}/>
-      <div className="card">
-        <MyComponents.User
-            user={this.props.data.user}
-            loginAction={this.props.actions.login}
-            logoutAction={this.props.actions.logout}/>
+    return (
+      <div>
+        <MyComponents.NavBar actions={this.props.actions}/>
+        <div className="collection">
+          <div className="card">
+            <MyComponents.User
+              user={this.props.data.user}
+              loginAction={this.props.actions.login}
+              logoutAction={this.props.actions.logout}/>
+          </div>
+          <div className="card">
+            <MyComponents.MapView
+              providers={this.props.data.providers}
+              center={this.props.data.center}
+              user={this.props.data.user}
+              setUserLocationAction={this.props.actions.setUserLocation}/>
+          </div>
+        </div>
       </div>
-      <div className="card">
-        <MyComponents.MapView
-            providers={this.props.data.providers}
-            center={this.props.data.center}
-            user={this.props.data.user}
-            setUserLocationAction={this.props.actions.setUserLocation}/>
-      </div>
-    </div>
+      )
   }
 }
 
