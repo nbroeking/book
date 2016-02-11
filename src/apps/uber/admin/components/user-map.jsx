8f0d70ecf6a9f6a13d49fa1b68d7bb842bf7094c
BenedictRM <mehringr@gmail.com>
2016-02-10 21:23:39 -0700

    initial admin update
    here

--- src/apps/uber/admin/components/user-map.jsx
+++ src/apps/uber/admin/components/user-map.jsx
@@ -1,10 +1,17 @@
+const {Map, Marker, CircleMarker, Popup, TileLayer, MapLayer}  = window.ReactLeaflet
+
 class UserMap extends React.Component {
   render(){
-    return <div>
-      <div>ToDo: Show All Users on a Map
-        <pre>{JSON.stringify(this.props.users)}</pre>
-      </div>
-    </div>
+    return  <Map center={this.props.center}
+          zoom={13}
+          onLeafletClick={this.handleLeafletClick.bind(this)}>
+        <TileLayer
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {providerElements}
+        {userElement}
+      </Map>
   }
 }
 
