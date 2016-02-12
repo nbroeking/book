ffd02b92fba61eeb4e4dfb0a379ff5aee8f972f0
Kierancz <kieran.czerwinski@colorado.edu>
2016-02-11 19:01:08 -0700

    cleaned a bit, fixed containers and logo link

--- src/apps/uber/admin/components/app.jsx
+++ src/apps/uber/admin/components/app.jsx
@@ -1,28 +1,29 @@
 class App extends React.Component {
   render(){
-  
     var userElements = this.props.data.users.map(function(u,i){
       return <MyComponents.UserList user={u} key={i}/>
     })
     
     return <div>
 	  <MyComponents.NavBar data={this.props.data} actions={this.props.actions}/>
-	  <div className="card">
-		<MyComponents.UserMap users={this.props.data.users}
-		  providers={this.props.data.providers}
-		  center={this.props.data.center}
-		  user={this.props.data.user}
-		  setUserLocationAction={this.props.actions.setUserLocation}/>
-	  </div>
+    <div className="container">
+  	  <div className="card">
+  		  <MyComponents.UserMap users={this.props.data.users}
+  		  providers={this.props.data.providers}
+  		  center={this.props.data.center}
+  		  user={this.props.data.user}
+  		  setUserLocationAction={this.props.actions.setUserLocation}/>
+  	  </div>
 	  
       <h3>Users List:</h3>
-	  <hr></hr>
-	  <div className="row">
-          {userElements}
-	  </div>
+  	  <hr></hr>
+  	  <div className="row">
+        {userElements}
+  	  </div>
 	  
       <MyComponents.ProviderMap users={this.props.data.providers}/>
       <MyComponents.ProviderList providers={this.props.data.providers}/>
+    </div>
     </div>
   }
 }
--- src/apps/uber/client/components/app.jsx
+++ src/apps/uber/client/components/app.jsx
@@ -3,21 +3,23 @@ class App extends React.Component {
     return (
       <div>
         <MyComponents.NavBar data={this.props.data} actions={this.props.actions}/>
-        <div className="collection">
-          <div className="card">
-            <div className="collection">
-            <MyComponents.User
-              user={this.props.data.user}
-              loginAction={this.props.actions.login}
-              logoutAction={this.props.actions.logout}/>
-          </div>
-          </div>
-          <div className="card">
-            <MyComponents.MapView
-              providers={this.props.data.providers}
-              center={this.props.data.center}
-              user={this.props.data.user}
-              setUserLocationAction={this.props.actions.setUserLocation}/>
+        <div className="container">
+          <div className="collection">
+            <div className="card">
+              <div className="collection">
+              <MyComponents.User
+                user={this.props.data.user}
+                loginAction={this.props.actions.login}
+                logoutAction={this.props.actions.logout}/>
+            </div>
+            </div>
+            <div className="card">
+              <MyComponents.MapView
+                providers={this.props.data.providers}
+                center={this.props.data.center}
+                user={this.props.data.user}
+                setUserLocationAction={this.props.actions.setUserLocation}/>
+            </div>
           </div>
         </div>
       </div>
