d2d475fd0c01d0b296eb32aeefeb15953253b2b2
BenedictRM <mehringr@gmail.com>
2016-02-17 20:05:03 -0700

    names for popup on client side
    here

--- src/apps/uber/client/components/map.jsx
+++ src/apps/uber/client/components/map.jsx
@@ -17,7 +17,7 @@ class MapView extends React.Component {
       var pos = [p.lat, p.lon];
       return <Marker position={pos} key={i} icon={provider_icon}>
         <Popup>
-          <span>{JSON.stringify(p)}</span>
+          <span>{p.name}</span>
         </Popup>
       </Marker>
     })
