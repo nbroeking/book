e819517d1ed0dbaf5415bc9ed5016c2ca546a6d8
Nicolas Broeking <nbroeking@me.com>
2016-02-19 14:28:07 -0700

    Fixed the script

--- simulate-providers.js
+++ simulate-providers.js
@@ -9,35 +9,118 @@ var city_location = {
 }
 
 var radius = 0.03
+var drivers={};
 
 // simualate a random person entering, staying for a duration, and leaving
 function simulate(){
+  var randnumber = Math.floor((Math.random() * 10) + 1);
 
-  // generate a random person with a random name,
-  // random location, and random duration
-  var name = random_name()
-  var restaurant = randRestaurant()
-  var duration = 1 + 5 * Math.random()
-  var lat = city_location.lat + radius * (Math.random() - 0.5) * 2
-  var lon = city_location.lon + radius * (Math.random() - 0.5) * 2
-  var person = {
-    name: name,
+  
+  if( randnumber < 7 && Object.keys(drivers).length < 15){  
+    // generate a random person with a random name,
+    // random location, and random duration
+    var name = random_name()
+    var restaurant = randRestaurant()
+    var duration = 1 + 5 * Math.random()
+    var lat = city_location.lat + radius * (Math.random() - 0.5) * 2
+    var lon = city_location.lon + radius * (Math.random() - 0.5) * 2
+    var person = {
+      name: name,
 	  restaurant: restaurant,
-    duration: duration,
-    lat: lat,
-    lon: lon
+      duration: duration,
+      lat: lat,
+      lon: lon,
+      direction: "up"
+    }
+
+    // simulate this person entering
+    enter(person)
+
+    // simulate this person leaving after 'duration' seconds
+    setTimeout(function(){
+      leave(person)
+    }, duration * 10000)
+
   }
 
-  // simulate this person entering
-  enter(person)
+  //Drive Everyone Around
+  for (var key in drivers) {
+  
+    randnumber = Math.floor((Math.random() * 10) + 1);
+    if (drivers.hasOwnProperty(key)) {
+      var driver = drivers[key];
+    
+      if( randnumber < 3){
+        turnRight(driver);
+        
+      }
+      else if( randnumber < 6){
+        turnLeft(driver); 
+      }
+      moveForward(driver);
+    }
+  }
+}
 
-  // simulate this person leaving after 'duration' seconds
-  setTimeout(function(){
-    leave(person)
-  }, duration * 1000)
+function moveForward( person){
+    if( person.direction == "up"){
+        person.lon += 0.003;
+    }
+    else if( person.direction == "right"){
+        person.lat += 0.003;
+    }
+    else if( person.direction == "down") {
+        person.lon -= 0.003;
+    }
+    else{
+        person.lat -= 0.003;
+    }
+
+    var ref = new Firebase("https://team-revolver.firebaseio.com/");
+    var usersRef = ref.child("providers");
+    usersRef.child(person.name).set({
+      name: person.name,
+	  restaurant: person.restaurant,
+      duration: person.duration,
+      lat: person.lat,
+      lon: person.lon,
+      directions: person.direction
+    });
 
 }
 
+function turnRight( person){
+    if( person.direction == "up"){
+        person.direction = "right";
+    }
+    else if( person.direction == "right"){
+        person.direction = "down";
+    }
+    else if( person.direction == "down") {
+        person.direction = "left";
+    }
+    else{
+        person.direction = "up";
+    }
+    var ref = new Firebase("https://team-revolver.firebaseio.com/providers/"+person.name+"/direction");
+    ref.set(person.direction);
+}
+function turnLeft( person){
+    if( person.direction == "up"){
+        person.direction = "left";
+    }
+    else if( person.direction == "left"){
+        person.direction = "down";
+    }
+    else if( person.direction == "down") {
+        person.direction = "right";
+    }
+    else{
+        person.direction = "up";
+    }
+    var ref = new Firebase("https://team-revolver.firebaseio.com/providers/"+person.name+"/direction");
+    ref.set(person.direction);
+}
 function randRestaurant(){
 	var restaurants = [
       'Take a Pizza My Heart, ltd.',
@@ -63,14 +146,19 @@ function enter(person){
 	  restaurant: person.restaurant,
     duration: person.duration,
     lat: person.lat,
-    lon: person.lon
+    lon: person.lon,
+    directions: person.direction
     });
+
+  drivers[person.name] = person;
+  
 }
 
 function leave(person){
   console.log('leave', person)  
   var ref = new Firebase("https://team-revolver.firebaseio.com/providers/"+person.name);
   ref.remove();
+  delete drivers[person.name]
 }
 
 function clear(){ 
@@ -81,5 +169,5 @@ function clear(){
 //This is so we always start fresh
 clear();
 
-// run every second
-setInterval(simulate, 2000)
+// run every half second
+setInterval(simulate, 1000);
